e0ba8a6f602b6eb4ca07a17ae6334d93a0cc05fd
penghuaping <lansheng228@163.com>
2023-03-09 20:16:12 +0800

    整理代码

--- 001/main.rs
+++ 001/main.rs
@@ -1,8 +1,3 @@
-use std::process::Command;
-
 fn main() {
-   println!("hello world~~");
-
-   // 命令提示符 pause
-   let _ = Command::new("cmd.exe").arg("/c").arg("pause").status();	
+    println!("hello world");
 }
