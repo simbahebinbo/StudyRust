a80b413a6c22f2d0b64f9552f8bfff6c94411ab0
penghuaping <lansheng228@163.com>
2023-03-10 11:17:19 +0800

    整理代码

--- 014/string2/src/main.rs
+++ 014/string2/src/main.rs
@@ -12,12 +12,14 @@ fn main() {
     println!("hello 堆上的指针 -> {:p}", hello.as_ptr());
     println!("hello 的容量 -> {}", hello.capacity());
     println!("hello 的字节长度 -> {}", hello.len());
-
     println!("hello 栈上的指针 -> {:p}", &hello);
 
-    // let hello_string = String::from("hello");
-    // let hello_str = &hello[1..];
-    // let hello_literal = "world";
-
-
+    let hello_string = String::from("hello");
+    let hello_str = &hello[1..];
+    let hello_literal = "world";
+    dbg!(hello_string);
+    dbg!(hello_str);
+    dbg!(hello_literal);
 }
+
+
